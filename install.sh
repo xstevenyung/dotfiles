46049fe865c477ec4a3fe376d59b463bbcae6eb7
Steven Yung <stevenyung@icloud.com>
2017-03-18 12:16:21 +0100

    Add brewCask install loop in install

--- install.sh
+++ install.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 declare -a brew=('adblock' 'bash' 'composer' 'elasticsearch@2.4' 'freetype' 'gettext' 'heroku' 'icu4c' 'jpeg' 'libpng' 'libxml2' 'mongodb' 'mysql' 'node' 'openssl' 'pcre' 'php56' 'php56-mongo' 'php70' 'php71' 'postgresql' 'readline' 'symfony-installer' 'unixodbc' 'watchman' 'wget' 'yarn' 'youtube-dl');
+declare -a brewCask=('atom' 'avast' 'calibre' 'discord' 'dropbox' 'evernote' 'firefox' 'flux' 'google-chrome' 'google-drive' 'google-hangouts' 'hyper' 'java' 'league-of-legends' 'postman' 'psequel' 'robomongo' 'sequel-pro' 'skype' 'slack' 'sourcetree' 'spotify' 'the-unarchiver' 'transmission' 'vlc');
 
 for i in "${brew[@]}"
 do
   :
   brew install $i
+done
+
+for i in "${brewCask[@]}"
+do
+	:
+brew cask install $i
 done
