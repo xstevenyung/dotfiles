ebaf9076505434f1e2fc20931195d3cae90dd36a
Steven Yung <stevenyung@icloud.com>
2017-03-18 12:12:47 +0100

    Add install.sh

---
+++ install.sh
@@ -0,0 +1,7 @@
+declare -a brew=('adblock' 'bash' 'composer' 'elasticsearch@2.4' 'freetype' 'gettext' 'heroku' 'icu4c' 'jpeg' 'libpng' 'libxml2' 'mongodb' 'mysql' 'node' 'openssl' 'pcre' 'php56' 'php56-mongo' 'php70' 'php71' 'postgresql' 'readline' 'symfony-installer' 'unixodbc' 'watchman' 'wget' 'yarn' 'youtube-dl');
+
+for i in "${brew[@]}"
+do
+  :
+  brew install $i
+done
