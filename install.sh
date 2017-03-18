028cd59d595bbc90e7d9399070d476e52da51758
Steven Yung <stevenyung@icloud.com>
2017-03-18 12:16:02 +0100

    Add shebang on install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 declare -a brew=('adblock' 'bash' 'composer' 'elasticsearch@2.4' 'freetype' 'gettext' 'heroku' 'icu4c' 'jpeg' 'libpng' 'libxml2' 'mongodb' 'mysql' 'node' 'openssl' 'pcre' 'php56' 'php56-mongo' 'php70' 'php71' 'postgresql' 'readline' 'symfony-installer' 'unixodbc' 'watchman' 'wget' 'yarn' 'youtube-dl');
 
 for i in "${brew[@]}"
