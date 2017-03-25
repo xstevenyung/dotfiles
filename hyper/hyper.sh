2f4e4236a284aa49bab4bc344a5dd84e6612b4e1
Steven Yung <stevenyung@icloud.com>
2017-03-25 11:29:00 +0100

    Add Hyper basic plugins

--- hyper/hyper.sh
+++ hyper/hyper.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-declare -a hpm=()
+declare -a hpm=('hyperterm-paste' 'hyper-statusline' 'hyperlinks' 'hyper-dark-dracula')
 
 brew cask install hyper
+npm install -g hpm-cli
 
 for i in "${hpm[@]}"
 do
