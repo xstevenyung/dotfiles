17c1f234f704155e666086cb13c4ae13eb61d36c
Steven Yung <stevenyung@icloud.com>
2017-04-08 16:42:00 +0200

    Add Emacs to install@brewCask

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 declare -a brew=('adblock' 'bash' 'composer' 'elasticsearch@2.4' 'freetype' 'gettext' 'heroku' 'icu4c' 'jpeg' 'libpng' 'libxml2' 'mongodb' 'mysql' 'node' 'openssl' 'pcre' 'php56' 'php56-mongo' 'php70' 'php71' 'postgresql' 'readline' 'symfony-installer' 'unixodbc' 'watchman' 'wget' 'yarn' 'youtube-dl');
-declare -a brewCask=('avast' 'calibre' 'discord' 'dropbox' 'evernote' 'firefox' 'flux' 'google-chrome' 'google-drive' 'google-hangouts' 'java' 'postman' 'psequel' 'robomongo' 'sequel-pro' 'skype' 'slack' 'sourcetree' 'spotify' 'the-unarchiver' 'transmission' 'vlc' 'imageoptim');
+declare -a brewCask=('avast' 'calibre' 'discord' 'dropbox' 'evernote' 'firefox' 'flux' 'google-chrome' 'google-drive' 'google-hangouts' 'java' 'postman' 'psequel' 'robomongo' 'sequel-pro' 'skype' 'slack' 'sourcetree' 'spotify' 'the-unarchiver' 'transmission' 'vlc' 'imageoptim' 'emacs');
 
 for i in "${brew[@]}"
 do
