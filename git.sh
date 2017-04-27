d0a7ad9e86aaca25f3af3313512118869c2b2761
Steven Yung <stevenyung@icloud.com>
2017-04-28 00:16:42 +0200

    Setup vi as git default editor

--- git.sh
+++ git.sh
@@ -3,3 +3,6 @@
 # Setup Git user config
 git config --global user.name "Steven Yung"
 git config --global user.email stevenyung@icloud.com
+
+# Setup vi as default editor
+git config --global core.editor vi
