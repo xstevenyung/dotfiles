82a6a6b8a5809d3efc5cbebde28975e882190654
Steven Yung <stevenyung@icloud.com>
2017-04-28 00:15:24 +0200

    Add oh-my-zsh.sh

---
+++ oh-my-zsh.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Define default Shell
+chsh -s $(which zsh)
+
+# Install Oh-My-Zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
