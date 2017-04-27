9d629701b7bdb2ac97338309a837d58033b7d619
Steven Yung <stevenyung@icloud.com>
2017-04-28 00:33:50 +0200

    Create zsh/ with zshrc.symlink

--- zsh/oh-my-zsh.sh
+++ zsh/oh-my-zsh.sh
@@ -5,3 +5,6 @@ chsh -s $(which zsh)
 
 # Install Oh-My-Zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# Setup zshrc
+ln -s ~/workspace/dotfiles/zsh/zshrc.symlink ~/.zshrc
