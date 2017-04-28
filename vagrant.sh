420566b9d90c6196bdd53a41b55e790a63b0bdaf
Steven Yung <stevenyung@icloud.com>
2017-04-28 10:33:16 +0200

    Update vagant@homestead

--- vagrant.sh
+++ vagrant.sh
@@ -2,7 +2,6 @@
 
 # Homestead
 vagrant box add laravel/homestead
-mkdir ~/Code/homestead
+git clone git@github.com:laravel/homestead.git ~/Code/homestead
 cd ~/Code/homestead
-vagrant init laravel/homestead
-vagrant up
+open https://scotch.io/tutorials/getting-started-with-laravel-homestead
