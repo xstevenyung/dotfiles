773acc9b3174e4e167afc766bb8b842e6dd0dd03
Steven Yung <stevenyung@icloud.com>
2017-04-29 23:03:16 +0200

    Create vagrant/ with cetna env

--- vagrant/vagrant.sh
+++ vagrant/vagrant.sh
@@ -5,3 +5,9 @@ vagrant box add laravel/homestead
 git clone git@github.com:laravel/homestead.git ~/Code/homestead
 cd ~/Code/homestead
 open https://scotch.io/tutorials/getting-started-with-laravel-homestead
+
+# CETNA
+mkdir ~/Code/cetna
+ln -s ~/Code/dotfiles/vagrant/cetna.symlink ~/Code/cetna/Vagrantfile
+cd ~/Code/cetna
+vagrant up
