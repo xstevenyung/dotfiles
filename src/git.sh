b915305d8c3be0f50db3b5090a9f2ccaefcc8988
Steven Yung <stevenyung@icloud.com>
2017-07-22 22:49:55 +0200

    Remove git.sh

--- src/git.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Setup Git user config
-git config --global user.name "Steven Yung"
-git config --global user.email stevenyung@icloud.com
-
-# Setup vi as default editor
-git config --global core.editor vi
