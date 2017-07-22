67e65db25b6d71dc68abff98b8f46d7270abf7d0
Steven Yung <stevenyung@icloud.com>
2017-07-22 22:49:36 +0200

    Add Git.php

--- src/Application.php
+++ src/Application.php
@@ -17,6 +17,8 @@ class Application
             (new Brew)->run();
         } else if ($this->cmd == 'cask') {
             (new Cask)->run();
+        } else if ($this->cmd == 'cask') {
+            (new Git)->run();
         }
     }
 }
---
+++ src/Git.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Dotfiles;
+
+class Git
+{
+    function run()
+    {
+        # Setup Git user config
+        exec('git config --global user.name "Steven Yung"');
+        exec('git config --global user.email stevenyung@icloud.com');
+
+        # Setup vi as default editor
+        exec('git config --global core.editor vi');
+
+    }
+}
