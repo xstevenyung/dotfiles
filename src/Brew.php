afae3f69ac661982ae20ec19346d4f06e01a9367
Steven Yung <stevenyung@icloud.com>
2017-08-29 11:44:44 +0900

    Move installer setup logic to Installer

--- src/Brew.php
+++ src/Brew.php
@@ -6,13 +6,6 @@ use Symfony\Component\Yaml\Yaml;
 
 class Brew extends Installer
 {
-    public function __construct()
-    {
-        $this->cmd = $this->cmd();
-        $this->file = file_get_contents(config_path($this->file()));
-        $this->packages = Yaml::parse($this->file);
-    }
-
     protected function cmd()
     {
         return 'brew install';
--- src/Installer.php
+++ src/Installer.php
@@ -6,6 +6,10 @@ abstract class Installer
 {
     public function run()
     {
+        $this->cmd = $this->cmd();
+        $this->file = file_get_contents(config_path($this->file()));
+        $this->packages = Yaml::parse($this->file);
+        
         foreach ($this->packages as $package) {
             exec("{$this->cmd} {$package}");
         }
