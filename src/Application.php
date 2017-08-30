e6bea870e519e7e5cb528c3b5d2c03829cfa48b4
Steven Yung <stevenyung@icloud.com>
2017-08-30 23:42:46 +0900

    Limit entire application to Environment

--- src/Application.php
+++ src/Application.php
@@ -11,41 +11,41 @@ class Application
     private $climate;
     private $installer;
 
-    function __construct(CLImate $climate)
+    public function __construct(CLImate $climate)
     {
         $this->climate = $climate;
         $this->cmd = $this->climate->arguments->get('install');
-        $this->installer = [
+        $this->environment = [
             'atom' => Environments\Atom::class,
             'zsh' => Environments\Zsh::class,
 
-            'git' => Configurators\Git::class,
-            'macos' => Configurators\MacOS::class,
-
-            // 'atom' => Installers\Atom::class,
-            'brew' => Installers\Brew::class,
-            'cask' => Installers\Cask::class,
-            'java' => Installers\Java::class,
-            'javascript' => Installers\Javascript::class,
-
-            // 'atom' => Symlinkers\Atom::class,
-            'spacemacs' => Symlinkers\Spacemacs::class,
-            // 'zsh' => Symlinkers\Zsh::class,
+            // 'git' => Configurators\Git::class,
+            // 'macos' => Configurators\MacOS::class,
+            //
+            // // 'atom' => Installers\Atom::class,
+            // 'brew' => Installers\Brew::class,
+            // 'cask' => Installers\Cask::class,
+            // 'java' => Installers\Java::class,
+            // 'javascript' => Installers\Javascript::class,
+            //
+            // // 'atom' => Symlinkers\Atom::class,
+            // 'spacemacs' => Symlinkers\Spacemacs::class,
+            // // 'zsh' => Symlinkers\Zsh::class,
         ];
     }
 
     function run()
     {
         if (!$this->cmd) {
-            $this->cmd = $this->climate->radio('Choose a installer to execute:', array_keys($this->installer))->prompt();
+            $this->cmd = $this->climate->radio('Choose the environment to setup:', array_keys($this->environment))->prompt();
         }
 
-        if (!array_key_exists($this->cmd, $this->installer))
+        if (!array_key_exists($this->cmd, $this->environment))
         {
             $this->climate->to('error')->red("Installer {$this->cmd} doesn't exists.");
             return;
         }
 
-        (new $this->installer[$this->cmd])->run();
+        (new $this->environment[$this->cmd])->run();
     }
 }
