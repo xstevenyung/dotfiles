9467061bbee95d2586ef2bf75cbda7f9ce3a5333
Steven Yung <stevenyung@icloud.com>
2017-09-01 10:35:51 +0900

    Split Traits\ReadYaml@read and readConfig

--- src/Traits/ReadYaml.php
+++ src/Traits/ReadYaml.php
@@ -8,8 +8,13 @@ trait ReadYaml
 {
     public static function read($file)
     {
-        $file = file_get_contents(config_path($file));
+        $file = file_get_contents($file);
 
         return Yaml::parse($file);
+    }
+
+    public static function readConfig($file)
+    {
+        return self::read(config_path($file));
     }
 }
