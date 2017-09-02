f03faa35533c6b68be82a9c32b62137094f51c46
Steven Yung <stevenyung@icloud.com>
2017-09-02 22:05:59 +0900

    Clean up old files

--- src/php/unlink.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-brew unlink php54
-brew unlink php55
-brew unlink php56
-brew unlink php70
-brew unlink php71
--- src/php/valet/init.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Install
-composer global require laravel/valet
-~/.composer/vendor/bin/valet install
-
-# Symlink
-rm ~/.valet/config.json
-ln -fs ~/Code/dotfiles/php/valet/config.json.symlink ~/.valet/config.json
