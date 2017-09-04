8e396cc2188d2587ee3078d98290e6d0b8292076
Steven Yung <stevenyung@icloud.com>
2017-09-04 22:20:32 +0900

    Replae league/CLImate by Symfony/Console components

---
+++ src/Commands/SetupCommand.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace Dotfiles\Commands;
+
+use Exception;
+use Dotfiles\Environments;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class SetupCommand extends Command
+{
+    private function environments()
+    {
+        return [
+            'atom' => Environments\Atom::class,
+            'brew' => Environments\Brew::class,
+            'cask' => Environments\Cask::class,
+            'git' => Environments\Git::class,
+            'macos' => Environments\MacOS::class,
+            'php' => Environments\PHP::class,
+            'valet' => Environments\Valet::class,
+            'zsh' => Environments\Zsh::class,
+        ];
+    }
+
+    protected function configure()
+    {
+        $this
+            ->setName('setup')
+            ->setDescription('Setup one or more environments')
+            ->addArgument('environment', InputArgument::REQUIRED, 'What environments you want to setup?');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $io = new SymfonyStyle($input, $output);
+
+        $environment = $input->getArgument('environment');
+
+        if (in_array($environment, array_keys($this->environments())))
+        {
+            $this->environment($environment)->run();
+
+            return;
+        }
+
+        if ('*' === $environment) {
+            foreach ($this->environments() as $environment) {
+                $environment->run();
+            }
+
+            return;
+        }
+
+        $io->error("Environment not found: {$environment}");
+    }
+
+    private function environment($name)
+    {
+        $environments = $this->environments();
+
+        $env = $environments[$name];
+
+        return (new $env);
+    }
+}
