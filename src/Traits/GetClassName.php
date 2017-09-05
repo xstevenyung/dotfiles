0003e6c36b1b8a86fb5557b185e9f913db7e8299
Steven Yung <stevenyung@icloud.com>
2017-09-05 23:10:15 +0800

    Normalize Traits\GetClassName

--- src/Traits/GetClassName.php
+++ src/Traits/GetClassName.php
@@ -2,10 +2,12 @@
 
 namespace Dotfiles\Traits;
 
+use ReflectionClass;
+
 trait GetClassName
 {
     protected function getShortName()
     {
-        return (new \ReflectionClass($this))->getShortName();
+        return (new ReflectionClass($this))->getShortName();
     }
 }
