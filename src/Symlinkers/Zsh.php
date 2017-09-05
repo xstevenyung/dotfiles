0a5fb457e25567805d56bc05d837174962f92fec
Steven Yung <stevenyung@icloud.com>
2017-09-05 13:46:57 +0900

    Fix Symlinkers\Zsh@file

--- src/Symlinkers/Zsh.php
+++ src/Symlinkers/Zsh.php
@@ -6,7 +6,7 @@ class Zsh extends Symlinker
 {
     protected function file()
     {
-        return config_path('symlinks/zshrc.symlink');
+        return resources_path('symlinks/zshrc.symlink');
     }
 
     protected function destination()
